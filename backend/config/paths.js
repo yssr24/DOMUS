0b263a8e427b903e328841411c867b0bbd107225
Yasser Pandapatan <114791896+yssr24@users.noreply.github.com>
2025-10-02 01:29:58 +0800

    edit code

--- backend/config/paths.js
+++ backend/config/paths.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  emailTemplate: path.join(__dirname, './templates/verification.html'),
-  logo: path.join(__dirname, './assets/domus.png'),
-  forgotPasswordTemplate: path.join(__dirname, './templates/forgotpassword.html'),
-  createdProjectTemplate: path.join(__dirname, './templates/createdProject.html')
+  emailTemplate: path.join(__dirname, '../templates/verification.html'),
+  logo: path.join(__dirname, '../assets/domus.png'),
+  forgotPasswordTemplate: path.join(__dirname, '../templates/forgotpassword.html'),
+  createdProjectTemplate: path.join(__dirname, '../templates/createdProject.html')
 }
