225be0a073c1da43cfc6f1f46955fde88cd3355b
Yasser Pandapatan <114791896+yssr24@users.noreply.github.com>
2025-10-01 13:28:35 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const admin = require("firebase-admin");
+const admin = require("./config/database");
 const userRoutes = require('./router/userRoutes');
 const adminRoutes = require('./router/adminRoutes')
 
