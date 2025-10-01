d1ff15b808d4cda694538c1ad10cbaf11057f6fc
Yasser Pandapatan <114791896+yssr24@users.noreply.github.com>
2025-10-02 03:00:34 +0800

    edit code

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,11 @@
 // backend/config/database.js
 const admin = require("firebase-admin");
 
-// Parse the service account JSON from env
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Initialize Firebase Admin SDK (only once)
+// Convert the \\n back to real newlines
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
