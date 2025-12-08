ba33138fbb9e5683df7103426b139ee5aee430cb
domusarchitecture92-droid <domus.architecture92@gmail.com>
2025-12-09 05:58:22 +0800

    update

--- backend/config/appConfig.js
+++ backend/config/appConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-  FRONTEND_BASE_URL: 'https://www.domus.systems', // Change to your frontend domain in production
-  BACKEND_BASE_URL: 'https://domus.up.railway.app'   // Change to your backend domain in production
+  FRONTEND_BASE_URL: 'https://www.domus.systems', 
+  BACKEND_BASE_URL: 'https://domus.up.railway.app'
 }
