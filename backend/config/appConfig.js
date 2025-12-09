359bdabed512cfe32f19f67291fa03ca2cfe1eeb
domusarchitecture92-droid <domus.architecture92@gmail.com>
2025-12-10 06:45:42 +0800

    update

--- backend/config/appConfig.js
+++ backend/config/appConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-  FRONTEND_BASE_URL: 'https://www.domus.systems', 
-  BACKEND_BASE_URL: 'https://domus.up.railway.app'
+  FRONTEND_BASE_URL: 'https://www.domus.systems' || 'http://localhost:5173', 
+  BACKEND_BASE_URL: 'https://domus.up.railway.app' || 'http://localhost:5000'
 }
